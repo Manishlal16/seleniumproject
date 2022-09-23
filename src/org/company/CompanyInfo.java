12f5e16f0011a2d85255836f3afb6d33d1bd7ecc
Manishlal16 <manishlal005@gmail.com>
2022-09-23 15:14:30 +0530

    message1

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,20 @@
+package org.company;
+
+public class CompanyInfo {
+	public void CompanyName() {
+		System.out.println("CTS");
+	}
+public void CompanyId() {
+	System.out.println("12345");
+}
+public void CompanyAddress() {
+	System.out.println("xxxx");
+}
+public static void main(String[] args) {
+	CompanyInfo Details = new CompanyInfo();
+	Details.CompanyName();
+	Details.CompanyId();
+	Details.CompanyAddress();
+	
+}
+}
